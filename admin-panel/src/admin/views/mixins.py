2166fa72c8d24ea759685c421a82db92721002dc
hasansezertasan <hasansezertasan@gmail.com>
2024-01-28 00:16:15 +0300

    Formatted, pre-commit initialized.

--- admin-panel/src/admin/views/mixins.py
+++ admin-panel/src/admin/views/mixins.py
@@ -1,21 +1,24 @@
-from pydantic.fields import FieldInfo
 from typing import Dict, List
+
 from pydantic import BaseModel
+from pydantic.fields import FieldInfo
 
 
 class ModelView:
-    def __init__(self, model):
-        self.model: BaseModel = model
-        self.model_fields: Dict[str, FieldInfo] = self.model.model_fields
-    
-    def definitions(self):
-        definitions: List[tuple[str, dict]] = []
-        for k, v in self.model_fields.items():
-            data = v.json_schema_extra or {}
-            data.update({
-                "required": v.is_required(),
-                "description": v.description,
-                "annotation": v.annotation,
-            })
-            definitions.append((k, data))
-        return definitions
+	def __init__(self, model):
+		self.model: BaseModel = model
+		self.model_fields: Dict[str, FieldInfo] = self.model.model_fields
+
+	def definitions(self):
+		definitions: List[tuple[str, dict]] = []
+		for k, v in self.model_fields.items():
+			data = v.json_schema_extra or {}
+			data.update(
+				{
+					"required": v.is_required(),
+					"description": v.description,
+					"annotation": v.annotation,
+				}
+			)
+			definitions.append((k, data))
+		return definitions
--- hello-world/main.py
+++ hello-world/main.py
@@ -4,28 +4,27 @@ from fastui import AnyComponent, FastUI, prebuilt_html
 from fastui import components as c
 from fastui.events import GoToEvent
 
-
 app = FastAPI()
 
 
 @app.get("/api/", response_model=FastUI, response_model_exclude_none=True)
 def page() -> list[AnyComponent]:
-    return [
-        c.PageTitle(text="Title: Hello World!"),
-        c.Navbar(
-            title="Navbar Title: Hello World!",
-            title_event=GoToEvent(url="/"),
-        ),
-        c.Page(
-            components=[
-                c.Heading(text="H1 Header: Hello World"),
-                c.Paragraph(text="Paragraph: This is a simple demo of FastUI."),
-            ],
-        ),
-    ]
+	return [
+		c.PageTitle(text="Title: Hello World!"),
+		c.Navbar(
+			title="Navbar Title: Hello World!",
+			title_event=GoToEvent(url="/"),
+		),
+		c.Page(
+			components=[
+				c.Heading(text="H1 Header: Hello World"),
+				c.Paragraph(text="Paragraph: This is a simple demo of FastUI."),
+			],
+		),
+	]
 
 
 @app.get("/{path:path}")
 async def html_landing() -> HTMLResponse:
-    """Simple HTML page which serves the React app, comes last as it matches all paths."""
-    return HTMLResponse(prebuilt_html(title="FastUI Demo"))
+	"""Simple HTML page which serves the React app, comes last as it matches all paths."""
+	return HTMLResponse(prebuilt_html(title="FastUI Demo"))
