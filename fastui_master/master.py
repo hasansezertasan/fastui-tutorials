d7ce3f84612ab27526f4cfd78da7e63bb1160fc6
hasansezertasan <hasansezertasan@gmail.com>
2024-01-27 23:42:17 +0300

    Sketch for reusable Master Page (Layout)

---
+++ fastui_master/master.py
@@ -0,0 +1,102 @@
+from dataclasses import dataclass
+from typing import Any, Union
+
+from fastui import AnyComponent
+from fastui import components as c
+from fastui.events import GoToEvent
+
+
+def Master(*components: AnyComponent, title: str | None = None) -> list[AnyComponent]:
+    return [
+        c.PageTitle(text="Random Number Generator"),
+        c.Navbar(
+            title="Still Random Number Generator",
+            title_event=GoToEvent(url="/"),
+        ),
+        c.Page(
+            components=[
+                *((c.Heading(text=title),) if title else ()),
+                *components,
+            ],
+        ),
+    ]
+
+
+def Master(
+    *components: AnyComponent,
+    title: str | None = None,
+) -> list[AnyComponent]:
+    return [
+        c.PageTitle(text=f"FastUI Admin Demo — {title}" if title else "FastUI Admin Demo"),
+        c.Navbar(
+            title="FastUI Demo",
+            title_event=GoToEvent(url="/"),
+            links=[
+                c.Link(
+                    components=[c.Text(text='Users')],
+                    on_click=GoToEvent(url='/user'),
+                    active='startswith:/user',
+                ),
+                c.Link(
+                    components=[c.Text(text='Posts')],
+                    on_click=GoToEvent(url='/post'),
+                    active='startswith:/post',
+                ),
+            ],
+        ),
+        c.Page(
+            components=[
+                *((c.Heading(text=title),) if title else ()),
+                *components,
+            ],
+        ),
+        c.Footer(
+            extra_text="FastUI Admin Demo",
+            links=[
+                c.Link(
+                    components=[c.Text(text="Github")],
+                    on_click=GoToEvent(url="https://github.com/hasansezertasan/FastUI-Admin"),
+                ),
+                c.Link(
+                    components=[c.Text(text="PyPI")], on_click=GoToEvent(url="https://pypi.org/project/fastui-admin/")
+                ),
+            ],
+        ),
+    ]
+
+
+@dataclass
+class MasterView:
+    title: Union[str, None] = "FastUI Admin"
+
+    def render(self, *components: AnyComponent, title: str | None = None) -> list[AnyComponent]:
+        return [
+            c.PageTitle(text=self.title),
+            c.Navbar(
+                title=self.title,
+                title_event=GoToEvent(url="/"),
+                links=[
+                    c.Link(
+                        components=[c.Text(text="Components")],
+                        on_click=GoToEvent(url="/components"),
+                        active="startswith:/components",
+                    ),
+                    c.Link(
+                        components=[c.Text(text="Tables")],
+                        on_click=GoToEvent(url="/table/cities"),
+                        active="startswith:/table",
+                    ),
+                    c.Link(
+                        components=[c.Text(text="Forms")],
+                        on_click=GoToEvent(url="/forms/login"),
+                        active="startswith:/forms",
+                    ),
+                ],
+            ),
+            c.Page(
+                components=[
+                    *((c.Heading(text=self.title),) if self.title else ()),
+                    *components,
+                ],
+            ),
+        ]
