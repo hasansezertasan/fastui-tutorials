4addfc10ae7f138487f68c985721aa510cb1fead
Hasan Sezer Taşan <13135006+hasansezertasan@users.noreply.github.com>
2024-05-21 01:16:01 +0300

    Add Example: Starlette Hello World (#13)
    * Starlette example added.
    
    * Update pre-built html title.

---
+++ starlette-fastui/README.md
@@ -0,0 +1,13 @@
+# Starlette and FastUI
+
+This is a simple example project that shows how to use FastUI with Starlette.
+
+Is it possible?
+
+Well README file at the FastUI repository says so, but we need to try it...
+
+## How to run
+
+```shell
+uvicorn starlette-fastui.main:app --reload
+```
---
+++ starlette-fastui/main.py
@@ -0,0 +1,26 @@
+# Copyright 2024 Hasan Sezer Taşan <hasansezertasan@gmail.com>
+# Copyright (C) 2024 <hasansezertasan@gmail.com>
+from __future__ import annotations
+
+from fastui import AnyComponent, FastUI, prebuilt_html
+from fastui import components as c
+from starlette.applications import Starlette
+from starlette.requests import Request
+from starlette.responses import HTMLResponse, JSONResponse
+from starlette.routing import Route
+
+app = Starlette()
+
+
+def page(request: Request) -> JSONResponse:
+    return JSONResponse(FastUI(root=[c.Heading(text="Hello World")]).model_dump())
+
+
+def root(request: Request) -> HTMLResponse:
+    """Simple HTML page which serves the React app, comes last as it matches all paths."""
+    return HTMLResponse(prebuilt_html(title="FastUI and Starlette Example"))
+
+
+app = Starlette(
+    routes=[Route("/", root, methods=["GET"]), Route("/api/", page, methods=["GET"])]
+)
